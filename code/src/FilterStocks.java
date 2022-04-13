b7005b658d6a3633240f0ccff537e98ebdca05c0
dyllanh <64151515+dyllanh@users.noreply.github.com>
2022-04-13 11:22:59 -0600

    Add stock filtering code

---
+++ code/src/FilterStocks.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class FilterStocks {
+	public Stock[] filter(Stock[] stocks, String metric, float min, float max){
+		List<Stock> returnlist = new ArrayList<Stock>();
+		
+		if(metric.equals("PE")) {
+			for(Stock s : stocks) {
+				if(s.getAveragePE() > min && s.getAveragePE() < max) {
+					returnlist.add(s);
+				}
+			}
+		} else if(metric.equals("DE")) {
+			for(Stock s : stocks) {
+				if(s.getAverageDE() > min && s.getAverageDE() < max) {
+					returnlist.add(s);
+				}
+			}
+		} else if(metric.equals("PG")) {
+			for(Stock s : stocks) {
+				if(s.getAveragePG() > min && s.getAveragePG() < max) {
+					returnlist.add(s);
+				}
+			}
+		} else if(metric.equals("PS")) {
+			for(Stock s : stocks) {
+				if(s.getAveragePS() > min && s.getAveragePS() < max) {
+					returnlist.add(s);
+				}
+			}
+		} else if(metric.equals("PB")) {
+			for(Stock s : stocks) {
+				if(s.getAveragePB() > min && s.getAveragePB() < max) {
+					returnlist.add(s);
+				}
+			}
+		}
+		
+		int len = returnlist.size();
+		Stock[] rl = new Stock[len];
+		
+		return(returnlist.toArray(rl));
+	}
+}
